d6619e4f1057f35bbf4e04f24736b60792ad2897
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2011-09-13 03:38:14 +0530

    Edited start.js via GitHub

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World Nariya - Continuous Deployment Node JS');
+	res.end('Hello World Nariya - Continuous Deployment Node JS :)');
 });
 
 var port = process.argv[2];
