9f202144e1108f37975f4e34696a7ac93c83f794
EC2 Default User <ec2-user@domU-12-31-39-00-B2-66.compute-1.internal>
2011-09-12 18:58:58 +0000

    edited on ec2

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World Nariya');
+	res.end('Hello World Nariya - The Distributed Deployer');
 });
 
 var port = process.argv[2];
